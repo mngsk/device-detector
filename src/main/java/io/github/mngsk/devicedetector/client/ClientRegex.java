51cdf22fb11944badcf402f8c480db450ebc363f
Arturo González <61335355+mngsk@users.noreply.github.com>
2020-03-02 16:30:58 -0600

    Only match if useragent begins with given regex or there is no letter before it

--- src/main/java/io/github/mngsk/devicedetector/client/ClientRegex.java
+++ src/main/java/io/github/mngsk/devicedetector/client/ClientRegex.java
@@ -19,7 +19,9 @@ public class ClientRegex {
 
 	public void setRegex(String regex) {
 		this.regex = regex;
-		this.pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		this.pattern = Pattern.compile(
+				"(?:^|[^A-Z0-9\\-_]|[^A-Z0-9\\-]_|sprd-)(?:" + regex + ")",
+				Pattern.CASE_INSENSITIVE);
 	}
 
 	public String getName() {
