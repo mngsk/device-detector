6502271df124dbd701d873ba88d7f99cca140ffe
Arturo González <61335355+mngsk@users.noreply.github.com>
2020-03-02 11:34:21 -0600

    Add general detection test

--- src/test/java/io/github/mngsk/devicedetector/DeviceDetectorTest.java
+++ src/test/java/io/github/mngsk/devicedetector/DeviceDetectorTest.java
@@ -1,15 +1,12 @@
 package io.github.mngsk.devicedetector;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import io.github.mngsk.devicedetector.DeviceDetector.DeviceDetectorBuilder;
-import io.github.mngsk.devicedetector.client.Client;
-import io.github.mngsk.devicedetector.client.browser.Browser;
-import io.github.mngsk.devicedetector.device.Device;
-import io.github.mngsk.devicedetector.operatingsystem.OperatingSystem;
 
 public class DeviceDetectorTest {
 
@@ -17,11 +14,11 @@ public class DeviceDetectorTest {
 
 	@BeforeAll
 	public static void beforeAll() {
-		DD = new DeviceDetectorBuilder().enableEverything().build();
+		DD = new DeviceDetectorBuilder().build();
 	}
 
 	@Test
-	public void nonNullBuild() {
+	public void nonNullDetector() {
 		assertNotNull(DD);
 	}
 
@@ -36,79 +33,10 @@ public class DeviceDetectorTest {
 	}
 
 	@Test
-	public void invalidUserAgent() {
+	public void nonNullUserAgent() {
 		Detection d = DD.detect("12345");
 
-		assertEquals("12345", d.getUserAgent());
-		assertFalse(d.getDevice().isPresent());
-		assertFalse(d.getOperatingSystem().isPresent());
-		assertFalse(d.getClient().isPresent());
-	}
-
-	@Test
-	public void magicMethods() {
-		String userAgent = "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.136 Mobile Safari/537.36";
-		Detection d = DD.detect(userAgent);
-
-		assertTrue(d.isSmartphone());
-		assertFalse(d.isFeaturePhone());
-		assertFalse(d.isTablet());
-		assertFalse(d.isPhablet());
-		assertFalse(d.isCarBrowser());
-		assertFalse(d.isSmartDisplay());
-		assertFalse(d.isTV());
-		assertFalse(d.isConsole());
-		assertFalse(d.isPortableMediaPlayer());
-		assertFalse(d.isCamera());
-
-		assertTrue(d.isBrowser());
-		assertFalse(d.isLibrary());
-		assertFalse(d.isMediaPlayer());
-		assertFalse(d.isMobileApp());
-		assertFalse(d.isPIM());
-		assertFalse(d.isFeedReader());
-	}
-
-	@Test
-	public void device() {
-		String userAgent = "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.136 Mobile Safari/537.36";
-		Detection d = DD.detect(userAgent);
-
-		assertTrue(d.getDevice().isPresent());
-
-		Device expected = new Device("smartphone", "Google", "Nexus 4");
-		assertEquals(expected, d.getDevice().get());
-	}
-
-	@Test
-	public void operatingSystem() {
-		String userAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
-		Detection d = DD.detect(userAgent);
-
-		assertTrue(d.getOperatingSystem().isPresent());
-
-		OperatingSystem expected = new OperatingSystem("Windows", "Windows",
-				"x64", "7");
-		assertEquals(expected, d.getOperatingSystem().get());
-	}
-
-	@Test
-	public void client() {
-		String userAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
-		Detection d = DD.detect(userAgent);
-
-		assertTrue(d.getClient().isPresent());
-
-		Client expected = new Browser("Internet Explorer", "9.0", null, null);
-		assertEquals(expected, d.getClient().get());
-	}
-
-	@Test
-	public void touchEnabled() {
-		String userAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; ARM; Trident/6.0; Touch; ARMBJS)";
-		Detection d = DD.detect(userAgent);
-
-		assertTrue(d.isTouchEnabled());
+		assertNotNull(d);
 	}
 
 }
