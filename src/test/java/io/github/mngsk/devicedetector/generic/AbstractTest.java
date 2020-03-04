7e4a0f9bdc49dca62262e432c2587461c205dca0
Arturo González <61335355+mngsk@users.noreply.github.com>
2020-03-03 18:35:04 -0600

    Add generic tests

---
+++ src/test/java/io/github/mngsk/devicedetector/generic/AbstractTest.java
@@ -0,0 +1,58 @@
+package io.github.mngsk.devicedetector.generic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.mngsk.devicedetector.Detection;
+import io.github.mngsk.devicedetector.DeviceDetector;
+
+public abstract class AbstractTest {
+
+	protected static List<GenericFixture> FIXTURES;
+
+	protected static DeviceDetector DD;
+
+	protected ObjectMapper objectMapper = new ObjectMapper()
+			.findAndRegisterModules();
+
+	public static Stream<GenericFixture> fixtureProvider() {
+		return FIXTURES.stream();
+	}
+
+	@ParameterizedTest
+	@MethodSource("fixtureProvider")
+	protected void fixtures(GenericFixture fixture) {
+		Detection d = DD.detect(fixture.getUserAgent());
+		assertEquals(fixture.getOperatingSystem(),
+				d.getOperatingSystem().orElse(null),
+				() -> debug(fixture.getUserAgent(),
+						fixture.getOperatingSystem(),
+						d.getOperatingSystem().orElse(null)));
+		assertEquals(fixture.getClient(), d.getClient().orElse(null),
+				() -> debug(fixture.getUserAgent(), fixture.getClient(),
+						d.getClient().orElse(null)));
+		assertEquals(fixture.getDevice(), d.getDevice().orElse(null),
+				() -> debug(fixture.getUserAgent(), fixture.getDevice(),
+						d.getDevice().orElse(null)));
+	}
+
+	public String debug(String userAgent, Object expected, Object actual) {
+		StringBuilder sb = new StringBuilder(userAgent);
+		try {
+			sb.append("\nexpected: ")
+					.append(this.objectMapper.writeValueAsString(expected));
+			sb.append("\n  actual: ")
+					.append(this.objectMapper.writeValueAsString(actual));
+		} catch (Exception e) {
+		}
+		return sb.toString();
+	}
+
+}
---
+++ src/test/java/io/github/mngsk/devicedetector/generic/OtherTest.java
@@ -0,0 +1,63 @@
+package io.github.mngsk.devicedetector.generic;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import io.github.mngsk.devicedetector.DeviceDetector.DeviceDetectorBuilder;
+import io.github.mngsk.devicedetector.client.Client;
+import io.github.mngsk.devicedetector.client.ClientMixin;
+import io.github.mngsk.devicedetector.device.Device;
+import io.github.mngsk.devicedetector.device.DeviceMixin;
+import io.github.mngsk.devicedetector.operatingsystem.OperatingSystem;
+import io.github.mngsk.devicedetector.operatingsystem.OperatingSystemMixin;
+
+public class OtherTest extends AbstractTest {
+
+	@BeforeAll
+	public static void beforeAll() {
+		ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		objectMapper
+				.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+		objectMapper.addMixIn(OperatingSystem.class,
+				OperatingSystemMixin.class);
+		objectMapper.addMixIn(Client.class, ClientMixin.class);
+		objectMapper.addMixIn(Device.class, DeviceMixin.class);
+
+		FIXTURES = new ArrayList<>();
+		List<String> fixtureFiles = Arrays.asList("fixtures/camera.yml",
+				"fixtures/car_browser.yml", "fixtures/console.yml",
+				"fixtures/desktop.yml", "fixtures/feature_phone.yml",
+				"fixtures/feed_reader.yml", "fixtures/mediaplayer.yml",
+				"fixtures/mobile_apps.yml", "fixtures/phablet.yml",
+				"fixtures/portable_media_player.yml",
+				"fixtures/smart_display.yml", "fixtures/smart_speaker.yml",
+				"fixtures/tv.yml");
+		for (String fixtureFile : fixtureFiles) {
+			InputStream inputStream = OtherTest.class.getClassLoader()
+					.getResourceAsStream(fixtureFile);
+			CollectionType listType = objectMapper.getTypeFactory()
+					.constructCollectionType(List.class, GenericFixture.class);
+			try {
+				List<GenericFixture> genericFixtures = objectMapper
+						.readValue(inputStream, listType);
+				FIXTURES.addAll(genericFixtures);
+			} catch (IOException e) {
+				throw new RuntimeException("Could not load " + fixtureFile, e);
+			}
+		}
+
+		DD = new DeviceDetectorBuilder().build();
+	}
+
+}
